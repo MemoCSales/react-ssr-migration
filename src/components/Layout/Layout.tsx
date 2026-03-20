7e5167e42d27656250e1168bf1ea1f7d2ade7fae
MemoCSales <mcruz-sa@student.42wolfsburg.de>
2026-03-20 01:58:38 +0100

    feat: Update components - Removed duplicated Section, Container, ThemeProvider components that were unnecessary at this point - Created Layout wrapper

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,24 +3,34 @@ import { ReactNode } from 'react';
 import './Layout.module.scss';
 
 interface SectionProps {
-	children: ReactNode;
-	className?: string;
+  children: ReactNode;
+  className?: string;
 }
 
 interface ContainerProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 export function Section({ children, className= '' }: SectionProps) {
-	return ( <section className={`kto-layout-section ${className}`}>
-			{children}
-		</section>
-	)
+  return ( <section className={`kto-layout-section ${className}`}>
+      {children}
+    </section>
+  )
 }
 
 export function Container({ children }: ContainerProps) {
-	return ( <div className="kto-layout-container">
-		{children}
-	</div>
-	)
+  return ( <div className="kto-layout-container">
+    {children}
+  </div>
+  )
+}
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Section>
+      <Container>
+        {children}
+      </Container>
+    </Section>
+  )
 }
